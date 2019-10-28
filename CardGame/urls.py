8b5dd254efc6a9b54ef07356598acda8e6fbdd89
Thun Thunkijjanukij <thunthunkijjanukij@Thuns-MacBook-Pro.local>
2019-10-28 11:34:36 +0700

    add path scoreboard

--- CardGame/urls.py
+++ CardGame/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('/playing/', views.playing, name='playing')
+    path('/scoreboard/', views.scoreboard, name='scoreboard')
 ]
