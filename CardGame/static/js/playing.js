357d0856b70242265002976a734bd5d47984d132
kornkritpawit <konsoo@hotmail.com>
2019-11-03 14:37:11 +0700

    fix code. put js from playing.js to the html

--- CardGame/static/js/playing.js
+++ CardGame/static/js/playing.js
@@ -1,17 +1,3 @@
-var timeleft = 3;
-document.getElementById("end").style.display = "none";
-var downloadTimer = setInterval(function(){
-  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Game over"
-    document.getElementById("button").style.display = "none";
-    document.getElementById("end").style.display = "inline";
-
-  }
-}, 1000);
-
 
 
 
