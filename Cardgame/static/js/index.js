aede04c9f07838d81ccbcefd0d0a5265922d5b2c
Thun Thunkijjanukij <thunthunkijjanukij@Thuns-MacBook-Pro.local>
2019-11-14 20:44:26 +0700

    fix conflict

---
+++ Cardgame/static/js/index.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").onclick = function() {btn()};
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function btn() {
+    $(".describe").addClass("fadeout");
+    await sleep(500);
+    $(".description").addClass("hide");
+    $(".start").addClass("hide");
+    $(".home").addClass("smaller");
+    await document.getElementById('#id_username').focus()
+}
---
+++ Cardgame/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+
+from . import views
+
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('playing/', views.playing, name='playing'),
+    path('logout/',views.views_logout, name='logout'),
+    # path('/scoreboard/', views.scoreboard, name='scoreboard')
+]
