0d13c62214ed06fa016273a69938a69de87a20af
Klan300 <nathun.thun@hotmail.com>
2019-11-14 22:00:36 +0700

    delete form.py

--- Cardgame/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        max_length=254,
-        widget=forms.TextInput(attrs={'class': "input-lg"}),
-    )
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': "input-lg"}))
