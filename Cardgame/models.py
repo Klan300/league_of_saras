500f5b4c162ef61c3f325ac72a8bdb5322a1e264
Klan300 <nathun.thun@hotmail.com>
2019-11-15 13:23:23 +0700

    add facebook icon

--- Cardgame/models.py
+++ Cardgame/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Deck(models.Model):
@@ -15,3 +16,9 @@ class Card(models.Model):
 
     def __str__(self):
         return self.card_name
+
+class Playerscore(models.Model):
+    deck = models.ForeignKey(Deck,on_delete=models.CASCADE)
+    time = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(default=0)
