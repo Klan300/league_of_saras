a3562d58b73de54ab7b71706079b89d133110c78
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-15 22:32:38 +0700

    Add path for merge

--- Cardgame/urls.py
+++ Cardgame/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('playing/', views.playing, name='playing'),
     path('setting/', views.setting, name='setting'),
+    path('logout/',views.views_logout, name='logout'),
     # path('/scoreboard/', views.scoreboard, name='scoreboard')
 ]
