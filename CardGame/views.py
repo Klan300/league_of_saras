2cfc34d73c0915a143fd431ef3dd1b1c61170080
Klan300 <nathun.thun@hotmail.com>
2019-11-18 15:42:36 +0700

    fix home page

--- CardGame/views.py
+++ CardGame/views.py
@@ -1,6 +1,7 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render 
+from .models import Deck
 
 
 def index(request):
@@ -10,4 +11,8 @@ def playing(request):
     return render(request,'Cardgame/playing.html')
 
 def home(request):
-    return render(request, 'Cardgame/home.html')
+    deck = Deck.objects.all()
+    context = {
+        'deck_list' : deck
+    }
+    return render(request, 'Cardgame/home.html',context)
