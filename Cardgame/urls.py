5457d226bebda1159399d7c4e4c6deeb5676800c
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-18 15:25:34 +0700

    Edit background prefix

--- Cardgame/urls.py
+++ Cardgame/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
+
+
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('playing/', views.playing, name='playing'),
+    path('logout/',views.views_logout, name='logout'),
     path('setting/', views.setting, name='setting'),
     # path('/scoreboard/', views.scoreboard, name='scoreboard')
 ]
