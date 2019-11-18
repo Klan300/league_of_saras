e5ceadc0d6b978e96f8b3a516ba7d45b7dabe244
Klan300 <nathun.thun@hotmail.com>
2019-11-18 14:06:34 +0700

    add new css

--- Cardgame/models.py
+++ Cardgame/models.py
@@ -13,13 +13,13 @@ class Deck(models.Model):
         return self.deck_name
 
 
-
 class Card(models.Model):
     deck = models.ForeignKey(Deck,on_delete=models.CASCADE)
     card_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.card_name
+
 
 class Playerscore(models.Model):
     deck = models.ForeignKey(Deck,on_delete=models.CASCADE)
