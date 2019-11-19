f8e02653d6b25572c6f38197768b939d2172705b
Klan300 <nathun.thun@hotmail.com>
2019-11-19 12:36:14 +0700

    change cardgame foldername

---
+++ Cardgame/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Deck(models.Model):
+    deck_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.deck_name
+
+
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck,on_delete=models.CASCADE)
+    card_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.card_name
---
+++ Cardgame/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+from Cardgame.models import Deck
+# from django.utils import simplejson
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the sarasss.")
+    
+def playing(request):
+    topic = Deck.objects.get(pk=1)
+    total_card = list(topic.card_set.all())
+    card_name = [i.card_name for i in total_card]
+    print(card_name)
+    # card_name = simplejson.dumps(card_name)
+    # card_name = json.dumps(card_name)
+    return render(request,'Cardgame/playing.html',{'topic':topic,'cards':card_name},
+    )
