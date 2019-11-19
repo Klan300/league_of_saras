da3e24824e16495a916a27fc8ad3b70faf1e2131
Klan300 <nathun.thun@hotmail.com>
2019-11-19 12:35:37 +0700

    fix Cardgame folder conflict

--- CardGame/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('playing/', views.playing, name='playing')
-    # path('scoreboard/', views.scoreboard, name='scoreboard')
-]
