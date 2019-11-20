3ed016bd176fd61731333d1bfd3276b02733dbbb
Thun Thunkijjanukij <44374820+Klan300@users.noreply.github.com>
2019-11-20 10:53:36 +0700

    Revert "Homepage"

--- Cardgame/urls.py
+++ Cardgame/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('playing/', views.playing, name='playing'),
-    path('home/', views.home, name='home'),
     path('logout/',views.views_logout, name='logout'),
+    
+    # path('/scoreboard/', views.scoreboard, name='scoreboard')
 ]
