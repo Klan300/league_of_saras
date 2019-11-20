6098fee6ad4be37abc31656faf1cce9b36288aea
Klan300 <nathun.thun@hotmail.com>
2019-11-20 21:44:18 +0700

    reset commnit

--- CardGame/urls.py
+++ CardGame/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('playing/', views.playing, name='playing'),
     path('logout/',views.views_logout, name='logout'),
-    
+    path('home/', views.home, name='home'),
     # path('/scoreboard/', views.scoreboard, name='scoreboard')
 ]
