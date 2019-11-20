82db6beab72f8a985b2c452df75d9a2e189c6d42
Klan300 <nathun.thun@hotmail.com>
2019-11-20 11:16:51 +0700

    add

--- Cardgame/views.py
+++ Cardgame/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render ,redirect
 from django.contrib.auth import logout
 from .models import Deck
-from .forms import LoginForm
 
 
 
