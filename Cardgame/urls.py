5a33d9918582a45141825f22ebfebdddd9859646
Klan300 <nathun.thun@hotmail.com>
2019-11-20 22:32:02 +0700

    add cardgame

---
+++ Cardgame/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('playing/', views.playing, name='playing'),
+    path('logout/',views.views_logout, name='logout'),
+    path('home/', views.home, name='home'),
+    path('setting/', views.setting, name='setting'),
+    # path('/scoreboard/', views.scoreboard, name='scoreboard')
+]
