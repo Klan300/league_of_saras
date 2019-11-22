5e3d042bde6e2e8730494fd207647995837c0cb7
kornkritpawit <konsoo@hotmail.com>
2019-11-22 14:20:58 +0700

    fix playpage

--- Cardgame/urls.py
+++ Cardgame/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('playing/', views.playing, name='playing'),
+    path('playing/<str:name>', views.playing, name='playing'),
     path('logout/',views.views_logout, name='logout'),
     path('home/', views.home, name='home'),
     path('setting/', views.setting, name='setting'),
