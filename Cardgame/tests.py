e5df2b3138d59466b964306f58853541757d260b
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-24 19:35:35 +0700

    Add test_deck_count

--- Cardgame/tests.py
+++ Cardgame/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Deck, Card, Playerscore
+
+class DeckModelTest(TestCase):
+    def setUp(self):
+        Deck.objects.create(deck_name='English')
+        Deck.objects.create(deck_name='Science')
+    
+    def test_deck_count(self):
+        self.assertEqual(2, Deck.objects.count())
+
