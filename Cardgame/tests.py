d6bf847f3bd98ed9d48ca70097987295a8dcc5e3
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-28 11:47:41 +0700

    Add card test

--- Cardgame/tests.py
+++ Cardgame/tests.py
@@ -4,9 +4,13 @@ from .models import Deck, Card, Playerscore
 
 class DeckModelTest(TestCase):
     def setUp(self):
-        Deck.objects.create(deck_name='English')
-        Deck.objects.create(deck_name='Science')
+        self.deck1 = Deck.objects.create(deck_name='English')
+        self.deck2 = Deck.objects.create(deck_name='Science')
+        self.card1 = Card.objects.create(deck=self.deck1, card_name='What is the alphabet?')
     
     def test_deck_count(self):
         self.assertEqual(2, Deck.objects.count())
 
+    def test_card_count(self):
+        english = Card.objects.get(deck=self.deck1)
+        self.assertEqual(english.deck, self.deck1)
