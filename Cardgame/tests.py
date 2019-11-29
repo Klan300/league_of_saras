a79193518f2b0b98927121c0f3b581a9fbec037a
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-29 15:42:13 +0700

    Add selenium with setUp and tearDown

--- Cardgame/tests.py
+++ Cardgame/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+from time import sleep
 
 from .models import Deck, Card, Playerscore
 
 class DeckModelTest(TestCase):
+    
     def setUp(self):
         self.deck1 = Deck.objects.create(deck_name='English')
         self.deck2 = Deck.objects.create(deck_name='Science')
@@ -14,3 +19,16 @@ class DeckModelTest(TestCase):
     def test_card_count(self):
         english = Card.objects.get(deck=self.deck1)
         self.assertEqual(english.deck, self.deck1)
+
+
+class TestListPage(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('Cardgame/chromedriver/chromedriver.exe')
+    
+    def tearDown(self):
+        self.browser.close()
+    
+    def test_no_projects_alert_is_displayed(self):
+        self.browser.get(self.live_server_url)
+        sleep(10)
