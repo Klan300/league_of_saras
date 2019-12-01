1f7118d9e2a7d07073ca9f5eb63486b3742ed3af
Klan300 <nathun.thun@hotmail.com>
2019-12-01 20:36:36 +0700

    fix bug

--- league_of_saras/settings.py
+++ league_of_saras/settings.py
@@ -143,5 +143,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "Cardgame/static/"
 # location where django collect all static files
