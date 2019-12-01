76bc797440cf623a0cb8acb3c5e805f344de268b
Klan300 <nathun.thun@hotmail.com>
2019-12-01 20:32:23 +0700

    test static

--- league_of_saras/settings.py
+++ league_of_saras/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 import django_heroku
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths insid py e the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -143,4 +143,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "Cardgame/static/"
 # location where django collect all static files
