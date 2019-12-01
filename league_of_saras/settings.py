8c42b680ddfda3bb6e0e91434f88a88ad4908b1a
Klan300 <nathun.thun@hotmail.com>
2019-12-01 20:34:27 +0700

    fix bug

--- league_of_saras/settings.py
+++ league_of_saras/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 # DEBUG = False
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['leagueofsaras.herokuapp.com']
 
 
 # Application definition
