c6fbe1a7896f2145ab2a9819a0f6dacf38957e23
Klan300 <nathun.thun@hotmail.com>
2019-12-07 17:46:47 +0700

    fix

--- league_of_saras/settings.py
+++ league_of_saras/settings.py
@@ -93,8 +93,8 @@ WSGI_APPLICATION = 'league_of_saras.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 # Password validation
