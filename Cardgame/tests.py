b18aa9092e18ff31bbf3ca75d416338ec32489c2
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-12-16 17:22:59 +0700

    test user

--- Cardgame/tests.py
+++ Cardgame/tests.py
@@ -7,6 +7,8 @@ from time import sleep
 from threading import Event
 from urllib.parse import urlparse
 
+from django.contrib.auth import get_user_model
+
 from .models import Deck, Card, Playerscore
 
 class DeckModelTest(TestCase):
@@ -60,3 +62,31 @@ class TestListPage(StaticLiveServerTestCase):
         self.browser.find_element_by_partial_link_text('Google').click()
         url = urlparse(self.browser.current_url)
         self.assertEquals(url.netloc, 'accounts.google.com')
+
+
+class UsersManagersTest(TestCase):
+    
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='normal', email='normal@user.com', password='foo')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(username='super', email='super@user.com', password='foo')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                username='super', email='super@user.com', password='foo', is_superuser=False)
+    
